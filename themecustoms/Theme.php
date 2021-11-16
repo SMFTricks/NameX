571f205462b006643574c3c27961baef7bcdf16d
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-15 19:48:14 -0600

    update
    signed-off-by: diego andrés <diegoandres_cortes@outlook.com>

--- themecustoms/Theme.php
+++ themecustoms/Theme.php
@@ -14,10 +14,34 @@ if (!defined('SMF'))
 
 class Theme
 {
-	protected $_lib_options;
+	/**
+	 * @var string The theme version
+	 */
+	public $_theme_version = '1.0';
+
+	/**
+	 * @var array The theme color variants (red, green, blue, etc)
+	 */
+	private $_theme_variants = [];
+
+	/**
+	 * @var array The theme modes (dark, light, etc.)
+	 */
+	private $_theme_modes = [];
+
+	/**
+	 * @var array The libraries or frameworks to load, populated in libOptions()
+	 */
+	private $_lib_options = [];
 
 	public function __construct()
 	{
+		// Main Settings
+		$this->startSettings();
+
+		// Load Theme Strings
+		loadLanguage('ThemeStrings/');
+
 		// Include any libraries or frameworks
 		$this->libOptions();
 
@@ -26,6 +50,34 @@ class Theme
 
 		// Load the JS
 		$this->addJS();
+
+		/** @TODO */
+		// Theme Variants
+
+		/** @TODO */
+		// Add Theme Settings (hook)
+	}
+
+	protected function startSettings()
+	{
+		global $settings;
+
+		// The version this template/theme is for. This should probably be the version of SMF it was created for.
+		$settings['theme_version'] = $this->_theme_version;
+
+		// Set the following variable to true if this theme wants to display the avatar of the user that posted the last and the first post on the message index and recent pages.
+		$settings['avatars_on_indexes'] = true;
+
+		// Set the following variable to true if this theme wants to display the avatar of the user that posted the last post on the board index.
+		$settings['avatars_on_boardIndex'] = true;
+
+		// This defines the formatting for the page indexes used throughout the forum.
+		$settings['page_index'] = themecustoms_page_index();
+
+		// Allow css/js files to be disabled for this specific theme.
+		// Add the identifier as an array key. IE array('smf_script'); Some external files might not add identifiers, on those cases SMF uses its filename as reference.
+		if (!isset($settings['disable_files']))
+			$settings['disable_files'] = array();
 	}
 
 	public function libOptions()
--- themecustoms/templates/theme.template.php
+++ themecustoms/templates/theme.template.php
@@ -7,20 +7,39 @@
  * @license MIT
  */
 
+ /**
+ * Initialize the customs template, and other setting that are mainly ignored, so I brought them over here.
+ */
 function template_customs_init()
 {
 	global $settings;
+
+	// The version this template/theme is for. This should probably be the version of SMF it was created for.
+	$settings['theme_version'] = true;
 
 	// Integration hooks, and other fun stuff
 	add_integration_function('integrate_load_theme', 'ThemeCustoms\Integration::initialize#', false, $settings['theme_dir'] . '/themecustoms/Integration.php');
 }
 
+function themecustoms_page_index()
+{
+	global $txt;
+
+	return array(
+		'extra_before' => '<span class="pages">' . $txt['pages'] . '</span>',
+		'previous_page' => '<span class="main_icons previous_page"></span>',
+		'current_page' => '<span class="current_page">%1$d</span> ',
+		'page' => '<a class="nav_page" href="{URL}">%2$s</a> ',
+		'expand_pages' => '<span class="expand_pages" onclick="expandPages(this, {LINK}, {FIRST_PAGE}, {LAST_PAGE}, {PER_PAGE});"> ... </span>',
+		'next_page' => '<span class="main_icons next_page"></span>',
+		'extra_after' => '',
+	);
+}
+
 function themecustoms_header()
 {
-
 }
 
 function themecustoms_footer()
 {
-	
 }
