4efcb0116cd2a9cf247fed9ee9bac4349e4c3e1e
Diego Andrés Cortés <diegoandres_cortes@outlook.com>
2024-06-18 00:19:48 -0600

    update theme init file

---
+++ themecustoms/Config.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * @package Theme Customs
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2023, SMF Tricks
+ * @license GNU GPLv3
+ */
+
+namespace ThemeCustoms;
+
+use ThemeCustoms\Theme\Init;
+
+class Config extends Init
+{
+	/**
+	 * @var string Theme Name
+	 */
+	protected string $name = 'NameX';
+
+	/**
+	 * @var string Theme Version
+	 */
+	protected string $version = '1.4';
+
+	/**
+	 * @var array Theme Author
+	 */
+	protected string $author = 'Diego Andrés';
+
+	/**
+	 * @var int Theme Author SMF ID
+	 */
+	protected int $authorID = 254071;
+
+	/**
+	 * @var string Theme Default Color
+	 */
+	protected string $color = '#567c8f';
+
+	/**
+	 * @var string GitHub URL
+	 */
+	protected string $github = 'https://github.com/SMFTricks/NameX';
+
+	/**
+	 * @var int SMF Customization Site ID
+	 */
+	protected int $customizationId = 3003;
+
+	/**
+	 * @var int Theme Support Topic ID
+	 */
+	protected int $customizationSupport = 583206;
+
+	/**
+	 * @var string Custom Suport URL
+	 */
+	protected string $supportURL = 'https://smftricks.com/index.php?topic=1040.0';
+
+	/**
+	 * @var array Color Variants
+	 */
+	public array $variants = [
+		'red',
+		'green',
+		'blue',
+		'yellow',
+		'purple',
+		'pink',
+	];
+
+	/**
+	 * @var bool Dark Mode
+	 */
+	public bool $darkMode = true;
+
+	/**
+	 * @var bool Add the like button to the quickbuttons
+	 */
+	public bool $quickLikes = true;
+
+	/**
+	 * @var bool Wheter to include jQuery UI
+	 */
+	public bool $jqueryUI = true;
+
+	/**
+	 * Init::loadHooks()
+	 * 
+	 * @return void
+	 */
+	public function loadHooks() : void
+	{
+		// Load fonts
+		add_integration_function('integrate_pre_css_output', __CLASS__ . '::fonts', false, __FILE__);
+	}
+
+	/**
+	 * Init::fonts()
+	 * 
+	 * Load some google fonts
+	 * 
+	 * @return void
+	 */
+	public static function fonts() : void
+	{
+		global $context, $settings;
+
+		// Loading locally?
+		if (empty($settings['st_fonts_source']))
+		{
+			// Load the fonts
+			loadCSSFile('font.css', ['order_pos' => -800]);
+			return;
+		}
+
+		// Passion One Font
+		if (empty($context['header_logo_url_html_safe']))
+			loadCSSFile('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap', ['external' => true, 'order_pos' => -800]);
+
+		// Lato Font
+		loadCSSFile('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap', ['external' => true, 'order_pos' => -800]);
+	}
+}
